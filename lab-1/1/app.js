78891ae9eeacd678e1f2b2e48a05e6c9371f437d
simianu-vlad-30022 <61598579+simianu-vlad-30022@users.noreply.github.com>
2021-03-06 14:26:45 +0200

    problema 1

--- lab-1/1/app.js
+++ lab-1/1/app.js
@@ -1,3 +1,21 @@
 console.log('Welcome to data transmission ');
 
-document.getElementById('message').innerHTML = 'Message from JavaScript';
+document.getElementById('message').innerHTML = 'Google';
+console.log('Hello');
+var user = { id : 1 , name :" Andrei ", age : 21 , };
+console.log(user.name,"id = ",user.id,"age = ",user.age);
+function print(message) {
+    console.log(message);
+}
+print('good evening');
+
+var password='1234';
+console.log(password=='123'? 'ALLOW':'DENY');
+
+if(password == '1234'){
+    console.log('permission accepted');
+}else {
+    console.log('permission denied');
+}
+
+$('#message2').html('this is the second message');
