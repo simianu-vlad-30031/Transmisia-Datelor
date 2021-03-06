e4020f8fa956d1bef68fbcd5829624dd988da895
simianu-vlad-30022 <61598579+simianu-vlad-30022@users.noreply.github.com>
2021-03-06 17:00:37 +0200

    problema 4

--- lab-1/3/appTest.js
+++ lab-1/3/appTest.js
@@ -2,7 +2,7 @@
 function test(){
     console.log(sum(1)==0?'Passed':'Failed');
     console.log(sum(2)==3?'Passed':'Failed');
-    console.log(sum(4)==10?'Passed':'Failed');
+    console.log(sum(3)==6?'Passed':'Failed');
     console.log(sum(0)=='n is undefined'?'Passed':'Failed');
     console.log(sum("string")=="not a number"?'it is a string':'it is a number');
     console.log(sum(25)==="not a number"?'it is a string':'it is a number');
